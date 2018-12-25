cd68b787eb2a36df9a6a19122837a2a9105c7adf
Ko Phyo <myohanhtet.ly@gmail.com>
2018-12-25 19:24:16 +0630

    added user

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/htbs/create';
 
     /**
      * Create a new controller instance.
