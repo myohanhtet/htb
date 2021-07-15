364ac8078b8c50f555bbefc52cd7e37aa759859c
myohanhtet <myohanhtet.ly@gmail.com>
2021-07-15 14:08:19 +0630

    change pdf save route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,7 +57,7 @@
     @if (Route::has('login'))
         <div class="top-right links">
             @auth
-                <a href="{{ url('/home') }}">Home</a>
+                <a href="{{ url('/admin/dashboard') }}">Dashboard</a>
             @else
                 <a href="{{ route('login') }}">Login</a>
                 @if (Route::has('register'))
